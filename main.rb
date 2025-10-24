869e020322aaf6a79d48adf44f138eb0842163c0
its-namami <164639260+its-namami@users.noreply.github.com>
2025-10-24 19:43:08 +0200

    feat: add support for capital letters, refactor

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'debug'
 
-def get_char_from_range(char_array, char_needle, shift)
+def get_char_from_arr(char_array, char_needle, shift)
   shift %= char_array.size
 
   case char_needle
@@ -16,16 +16,42 @@ def get_char_from_range(char_array, char_needle, shift)
       char_array.last + shift + 1
     end
   else
-    char_needle
+    false
   end
 end
 
-def encrypt(input, shift)
+def caesar_cipher(input, shift)
   lowercase_letters = ('a'.ord..'z'.ord).to_a
+  uppercase_letters = ('A'.ord..'Z'.ord).to_a
 
-  input.split('').each.with_object('') do |letter, result|
-    result << get_char_from_range(lowercase_letters, letter.ord, shift).chr
+  input.split('').map { |letter|
+    char_ascii = get_char_from_arr(lowercase_letters, letter.ord, shift) || get_char_from_arr(uppercase_letters, letter.ord, shift) || letter.ord
+
+    char_ascii.chr
+  }.join('')
+end
+
+def ask_text
+  print "Enter the text\n>>> "
+  text = gets.chomp
+  puts
+  text
+end
+
+def ask_shift
+  begin
+    print "Enter the shift\n>>> "
+    shift = gets.chomp
+    p shift.inspect
+
+    shift.split('').each {|char|
+      raise "err" unless ('0'..'9') === char || ['-', '.'].include?(char)
+    }.join('').to_i
+  rescue
+    puts "Only numbers allowed!"
+    retry
   end
 end
 
-puts encrypt(gets.chomp, gets.to_i)
+puts "Welcome to Caesar Cipher!"
+puts "Result:\n#{caesar_cipher(ask_text, ask_shift)}"
