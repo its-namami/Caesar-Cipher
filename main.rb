1a489f9a6d49613372bcf3d49ee2a39a92f2b8d9
its-namami <164639260+its-namami@users.noreply.github.com>
2025-10-29 21:56:54 +0100

    refactor: to community standards

--- main.rb
+++ main.rb
@@ -1,4 +1,15 @@
-require 'debug'
+# frozen_string_literal: true
+
+def char_shift_case(char_array, _char_needle, shift, char_shift)
+  case char_shift
+  when char_array.first..char_array.last
+    char_shift
+  when ->(ch) { ch > char_array.last }
+    char_shift - char_array.last + char_array.first - 1
+  else
+    char_array.last + shift + 1
+  end
+end
 
 def char_shift(char_array, char_needle, shift)
   shift %= char_array.size
@@ -7,14 +18,7 @@ def char_shift(char_array, char_needle, shift)
   when char_array.first..char_array.last
     char_shift = char_needle + shift
 
-    case char_shift
-    when char_array.first..char_array.last
-      char_shift
-    when -> (ch) {ch > char_array.last}
-      char_shift - char_array.last + char_array.first - 1
-    else 
-      char_array.last + shift + 1
-    end
+    char_shift_case(char_array, char_needle, shift, char_shift)
   else
     false
   end
@@ -24,32 +28,30 @@ def caesar_cipher(input, shift)
   lowercase_letters = ('a'.ord..'z'.ord).to_a
   uppercase_letters = ('A'.ord..'Z'.ord).to_a
 
-  input.split('').map { |letter|
-    char_ascii = char_shift(lowercase_letters, letter.ord, shift) || char_shift(uppercase_letters, letter.ord, shift) || letter.ord
+  input.split('').map do |letter|
+    char_ascii = char_shift(lowercase_letters, letter.ord,
+                            shift) || char_shift(uppercase_letters, letter.ord, shift) || letter.ord
 
     char_ascii.chr
-  }.join('')
+  end.join('')
 end
 
 def ask_text
   print "Enter the text\n>>> "
-  text = gets.chomp
-  text
+  gets.chomp
 end
 
 def ask_shift
-  begin
-    print "Enter the shift\n>>> "
-    shift = gets.chomp
+  print "Enter the shift\n>>> "
+  shift = gets.chomp
 
-    shift.split('').each {|char|
-      raise "err" unless ('0'..'9') === char || ['-', '.'].include?(char)
-    }.join('').to_i
-  rescue
-    puts "Only numbers allowed!"
-    retry
-  end
+  shift.split('').each do |char|
+    raise 'err' unless ('0'..'9').include?(char) || ['-', '.'].include?(char)
+  end.join('').to_i
+rescue StandardError
+  puts 'Only numbers allowed!'
+  retry
 end
 
-puts "Welcome to Caesar Cipher!"
+puts 'Welcome to Caesar Cipher!'
 puts "Result:\n#{caesar_cipher(ask_text, ask_shift)}"
